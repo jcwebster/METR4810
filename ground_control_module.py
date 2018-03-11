cad428bfd4454a3620ef9cb709856cb22f081551
John Webster <jcwebster@uwaterloo.ca>
2018-03-11 22:12:51 +1000

    crosshairs and text added to webcam videocapture, mode_select not yet defined

--- ground_control_module.py
+++ ground_control_module.py
@@ -1,8 +1,23 @@
 from __future__ import print_function
 import os
 import numpy as np
+import cv2
+import time
+import serial
 
-import cv2, time
+#configure serial connection
+# ser = serial.Serial(port='/dev/ttyUSB1',
+# 					baudrate=9600,
+# 					parity=serial.PARITY_ODD,
+# 					stopbits=serial.STOPBITS_TWO,
+# 					bytesize=serial.SEVENBITS
+# 					)
+# 
+# ser.open()
+# ser.isOpen()
+# 
+# input = 1
+print("Enter your purpose: \n\r")
 
 #1. Create an object. Zero for external camera
 video = cv2.VideoCapture(0)
@@ -12,18 +27,63 @@ if not success:
 	print("Camera open fail")
 
 # Global Variables
+font = cv2.FONT_HERSHEY_PLAIN
 a=0
 width = video.get(cv2.CAP_PROP_FRAME_WIDTH) #float
 height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)  #float
 
+#define crosshairs size
+lineLength = 25
+xL = int((width - lineLength) / 2)
+xR = int(xL + lineLength)
+xT = int((height - lineLength) / 2)
+xB = int(xT + lineLength)
+
+def manual_steer():
+	print("Steering mode\n")
+	
+def calibrate():
+	print("Calibration mode")
+	
+def power_cycle():
+	print("Power cycling mode")
+
+def save_image():
+	print("Capturing image...")
+	
+	
+def mode_select(argument):
+	switcher = {
+		1: manual_steer,
+		2: calibrate,
+		3: power_cycle,
+		4: save_image
+	}
+
 while video.isOpened():
 	
+	# #get keyboard input
+	# input = raw_input(">> ")
+	# if input == 'exit':
+	# 	ser.close()
+	# 	exit()
+	# else:
+	# 	#send character back out; or send character as argument to mode_select
+	# 	ser.write(input)
+	# 	out = ""
+	# 	time.sleep(1)
+	# 	while ser.inWaiting() > 0:
+	# 		out += ser.read(1)
+	# 	
+	# 	if out != "":
+	# 		print(">>" + out)
+	# 		
 	a = a + 1
 	
 	#3. Create a frame object.
 	check, frame = video.read()
 	
-	print ("width: " +str(width))
+	print ("width: " + str(width))
 	print ("height: " + str(height))
 	print(check)
 	print(frame) # representing image
@@ -32,12 +92,15 @@ while video.isOpened():
 	
 	#grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	
-	#4. Show the frame!! after drawing the line*
-	cv2.line(img=frame, pt1=(100, 100), pt2=(200, 100), color=(255,0,255), thickness=50, lineType=4, shift=0)
+	#4. Show the frame!! after drawing the lines*
+	cv2.line(img=frame, pt1=(xL, int(height/2)), pt2=(xR, int(height/2)), color=(0, 255, 0), thickness=1, lineType=8, shift=0)
+	cv2.line(img=frame, pt1=(int(width/2), xT), pt2=(int(width/2), xB), color=(0, 255, 0), thickness=1, lineType=8, shift=0)
+	cv2.putText(frame, "width: " +str(width), (10,455), 1, font, (255,255,255))
+	cv2.putText(frame, "height: " + str(height), (10,475), 1, font, (255,255,255))
 	cv2.imshow("Capturing", frame)
 
 	#5. For press any key to advance frame (milliseconds)
-	cv2.waitKey(0)
+	#cv2.waitKey(0)
 	
 	#7. For playing
 	key = cv2.waitKey(1)
